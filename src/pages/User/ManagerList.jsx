ad5d1e45afb111b36b0dbff302cd624674f9c523
abc <tianxiamall@163.com>
2021-07-19 18:54:05 +0800

    管理员页面

--- src/pages/User/ManagerList.jsx
+++ src/pages/User/ManagerList.jsx
@@ -1,13 +1,69 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import { Table } from "antd";
+import AdminAPI from "@/api/admin_api";
 export class ManagerList extends Component {
-    render() {
-        return (
-            <div>
-                User List Pages
-            </div>
-        )
-    }
+  state = {
+    show: false,
+    setShow: false,
+    currentPage: "",
+    records: [],
+    totalPage: "",
+    delUser: "",
+    err: "",
+    searchIp: "",
+  };
+  componentDidMount() {
+    this.initData();
+  }
+
+  initData = async () => {
+    let result = await AdminAPI.adminList();
+    this.setState({
+      records: result.data.records,
+      totalCount: result.data.totalCount,
+    });
+  };
+  managerDelete = (userCode) => {
+    console.log(userCode);
+  };
+  render() {
+    const columns = [
+      {
+        title: "工号",
+        dataIndex: "userCode",
+      },
+      {
+        title: "创建时间",
+        dataIndex: "createTime",
+      },
+      {
+        title: "操作",
+        dataIndex: "userCode",
+        render: (text) => (
+          <button
+            onClick={() => {
+              this.managerDelete(text);
+            }}
+          >
+            删除
+          </button>
+        ),
+      },
+    ];
+    return (
+      <Table
+        rowKey="userCode"
+        loading={this.state.loading}
+        pagination={{
+          defaultPageSize: 20,
+          total: this.state.totalCount,
+          showSizeChanger: false,
+        }}
+        dataSource={this.state.records}
+        columns={columns}
+      />
+    );
+  }
 }
 
-export default ManagerList
+export default ManagerList;
